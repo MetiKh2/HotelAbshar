8ec73b3dc6f30c84035a0c5012fdc2aeecab04e5
MetiKh2 <meti.kh4@gmail.com>
2021-09-08 19:05:04 +0430

    sec commit

--- EndPoint.Site/Startup.cs
+++ EndPoint.Site/Startup.cs
@@ -33,7 +33,7 @@ namespace EndPoint.Site
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddWebMarkupMin(options=>
+            //services.AddWebMarkupMin(options=>b
             //{
             //    options.AllowMinificationInDevelopmentEnvironment=true;
             //    options.AllowCompressionInDevelopmentEnvironment = true;
